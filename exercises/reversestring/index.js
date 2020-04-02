b917759009a12e044e678009eec3d407bd755abc
Ira <ira.d.sanchez@gmail.com>
2020-04-01 21:19:06 -0700

    Solution 1 reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //solution 1
+  // return str
+  //   .split("")
+  //   .reverse()
+  //   .join("");
+}
 
 module.exports = reverse;
