46aa08c1b59e98b33cc5d6d52a1b9a6060eefc49
Ira <ira.d.sanchez@gmail.com>
2020-04-01 21:27:37 -0700

    Add Solution 2 for Reversed String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -12,6 +12,16 @@ function reverse(str) {
   //   .split("")
   //   .reverse()
   //   .join("");
+
+  //solution 2
+  let reversed = ""; //create empty array called reversed
+
+  for (i in str) {
+    //for each character, add it to the start of the reversed array
+    reversed = str[i] + reversed;
+  }
+
+  return reversed; //return reversed variable
 }
 
 module.exports = reverse;
